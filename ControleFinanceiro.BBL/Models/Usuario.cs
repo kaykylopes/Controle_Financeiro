4d73ba31d75691e5122f8b9f453afa563a6fa303
kayky lopes <kaykylopes@hotmail.com>
2021-02-24 22:59:06 -0300

    primeiro commit

---
+++ ControleFinanceiro.BBL/Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControleFinanceiro.BBL.Models
+{
+    public class Usuario : IdentityUser<string>
+    {
+        public string CPF { get; set; }
+        public string Profissao { get; set; }
+        public byte[]   Foto { get; set; }
+        public virtual ICollection<Cartao> Cartaos { get; set; }
+        public virtual ICollection<Ganho> Ganhos { get; set; }
+        public virtual ICollection<Despesa> Despesas { get; set; }
+    }
+}
